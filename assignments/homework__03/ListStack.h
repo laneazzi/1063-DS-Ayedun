e1dbd6e804727d648795c27e6087b575dbea5a95
Lanre Ayedun <lanreayedunjr@gmail.com>
2019-10-29 09:33:29 -0500

    Create ListStack.h

---
+++ assignments/homework__03/ListStack.h
@@ -0,0 +1,74 @@
+/**
+ * ListStack.h
+ * 
+ * List stack definition file. This stack hold animal types.
+ * 
+ */
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Animal Type
+ */
+struct Animal {
+    string name;
+    double weight;
+    double scary;
+
+    // Default constructor
+    Animal() 
+    {
+        name = " ";
+        weight = 0.0;
+        scary = 99.0;
+    }
+
+     // Overloaded constructor
+    Animal(string n, double w, double s)
+  	{
+	      name = n;
+	      weight = w;
+              scary = s;
+	}
+
+};
+
+// Overload "<<" operator for animals
+ostream &operator<<(ostream &, const Animal *);
+
+/**
+ * Node type.
+ * 
+ * We seperate Animal from Node so we can return an "Animal"
+ * when we pop the stack, and don't have to return a "node"
+ */
+struct Node {
+    Animal *A;
+    Node *Next;
+
+    // Needs a default constructor
+
+    Node(Animal *a) {
+        A = a;
+        Next = NULL;
+    }
+};
+
+/**
+ * ListStack
+ * 
+ * List based stack implementation
+ * 
+ */
+class ListStack {
+private:
+    Node *Top;
+
+public:
+    ListStack(); // default constructor
+    ListStack(string, double ,double); // overloaded constructor
+    void Push(Animal *);  // stack gets Animals
+    Animal *Pop();        // Pop returns top animal
+    void Print();         // print stack for debugging
+};
